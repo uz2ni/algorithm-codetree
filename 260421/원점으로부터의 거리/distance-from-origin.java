299512dd3558dd5a6484b4ee5e2d47306980e799
uz2ni <uzini_@likelion.org>
2026-04-21 10:09:18 +0900

    Title: 원점으로부터의 거리, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/원점으로부터의 거리/distance-from-origin.java
+++ 260421/원점으로부터의 거리/distance-from-origin.java
@@ -5,19 +5,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         List<Point> list = new ArrayList<>();
-        int subX;
-        int subY;
-        int subDistance;
         for(int i=0; i<n; i++) {
-            subX = sc.nextInt();
-            subY = sc.nextInt();
-            // 절대값
-            subDistance = Math.abs(subX) + Math.abs(subY);
-            list.add(new Point(i+1, subX, subY, subDistance));
+            int subX = sc.nextInt();
+            int subY = sc.nextInt();
+            list.add(new Point(i+1, subX, subY));
         }
         
         // 절대값 가까운 순 > 번호 작은 순(기본)
-        list.sort((p1, p2) -> p1.distance - p2.distance);
+        list.sort((p1, p2) -> {
+            int d1 = Math.abs(p1.x) + Math.abs(p1.y);
+            int d2 = Math.abs(p2.x) + Math.abs(p2.y);
+            if(d1 != d2) {
+                return d1 - d2;
+            }
+            return p1.index - p2.index;
+        });
 
         for(int i=0; i<list.size(); i++) {
             System.out.println(list.get(i).index);
@@ -28,13 +30,11 @@ public class Main {
         int index;
         int x;
         int y;
-        int distance;
 
-        public Point(int index, int x, int y, int distance) {
+        public Point(int index, int x, int y) {
             this.index = index;
             this.x = x;
             this.y = y;
-            this. distance = distance;
         }
     }
 }
