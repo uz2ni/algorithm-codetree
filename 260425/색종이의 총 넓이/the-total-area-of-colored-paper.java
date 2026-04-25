dae3c1c547b889fe882e7653d234d8f4e252f3d1
uz2ni <uzini_@likelion.org>
2026-04-25 18:01:50 +0900

    Title: 색종이의 총 넓이, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260425/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int offset = 100;
+        final int MAX_VALUE = (offset*2)+1;
+        boolean[][] visited = new boolean[MAX_VALUE][MAX_VALUE];
+        int result = 0;
+
+        for(int i=0; i<N; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = x1+8;
+            int y2 = y1+8;
+
+            for(int a=y1; a<y2; a++) {
+                for(int b=x1; b<x2; b++) {
+                    if(!visited[b][a]) {
+                        visited[b][a] = true;
+                        result++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
