a960186eb7188d3941f45160e99fc8095ddd125a
uz2ni <uzini_@likelion.org>
2026-04-25 23:51:22 +0900

    Title: 선두를 지켜라 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260425/선두를 지켜라 3/keep-the-lead-3.java
+++ 260425/선두를 지켜라 3/keep-the-lead-3.java
@@ -3,55 +3,62 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // N, M 입력 (최대 1,000)
         int N = sc.nextInt();
         int M = sc.nextInt();
-
-        // 1. 오차 방지를 위해 long 배열 사용
-        long[] n = new long[1000001];
-        long[] m = new long[1000001];
         
-        int timeN = 0;
-        long sumN = 0;
-        for(int i=0; i<N; i++) {
+        // 전체 시간은 최대 1,000 * 1,000 = 1,000,000
+        // 정밀도와 안전성을 위해 long 배열 사용
+        long[] posA = new long[1000001];
+        long[] posB = new long[1000001];
+        
+        int timeA = 0;
+        long sumA = 0;
+        for(int i = 0; i < N; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
-            for(int j=0; j<t; j++) {
-                sumN += v;
-                n[timeN++] = sumN;
+            for(int j = 0; j < t; j++) {
+                sumA += v;
+                posA[timeA++] = sumA;
             }
         }
-
-        int timeM = 0;
-        long sumM = 0;
-        for(int i=0; i<M; i++) {
+        
+        int timeB = 0;
+        long sumB = 0;
+        for(int i = 0; i < M; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
-            for(int j=0; j<t; j++) {
-                sumM += v;
-                m[timeM++] = sumM;
+            for(int j = 0; j < t; j++) {
+                sumB += v;
+                posB[timeB++] = sumB;
             }
         }
-
-        // 두 사람 중 더 긴 시간을 기준으로 함 (보통은 같게 주어짐)
-        int totalHour = Math.max(timeN, timeM);
+        
+        // 문제에서 A와 B가 이동한 총 시간은 동일하다고 주어지는 경우가 많습니다.
+        int totalTime = timeA; 
         
         int cnt = 0;
-        int history = -1;
-
-        for(int i=0; i < totalHour; i++) {
+        int history = -1; // 이전 상태 (0: 공동, 1: A선두, 2: B선두)
+        
+        for(int i = 0; i < totalTime; i++) {
             int cur;
-            if(n[i] > m[i])      cur = 1; // A가 선두
-            else if(n[i] < m[i]) cur = 2; // B가 선두
-            else                 cur = 0; // 공동 선두
-
-            if(i == 0) {
+            if (posA[i] > posB[i]) cur = 1;      // {A}
+            else if (posA[i] < posB[i]) cur = 2; // {B}
+            else cur = 0;                        // {A, B}
+            
+            if (i == 0) {
+                // 시작 직후의 상태는 '변화'가 아니라 '초기 상태'로 설정
                 history = cur;
-            } else if(history != cur) {
-                cnt++;
-                history = cur;
+            } else {
+                // 이전 초의 명예의 전당 조합과 현재 초의 조합이 다르면 카운트
+                if (history != cur) {
+                    cnt++;
+                    history = cur;
+                }
             }
         }
-
+        
         System.out.println(cnt);
     }
 }
