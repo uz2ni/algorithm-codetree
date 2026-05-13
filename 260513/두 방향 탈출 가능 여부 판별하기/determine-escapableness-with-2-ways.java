afbd51ea0ae690ce79967e48e9394085601d16b6
uz2ni <uzini_@likelion.org>
2026-05-13 11:49:53 +0900

    [Passed] 두 방향 탈출 가능 여부 판별하기 (307ms, 21MB)

---
+++ 260513/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] grid = new int[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                grid[i][j] = sc.nextInt();
+        // Please write your code here.
+
+        int[][] visited = new int[n][m];
+        dfs(grid, 0, 0, n, m, visited);
+        
+        System.out.println(visited[n-1][m-1]);
+        
+    }
+
+    public static void dfs(int[][] grid, int x, int y, int n, int m, int[][] visited) {
+        
+        visited[x][y] = 1;
+
+        int[] dc = {0, 1}; // x,n 방향
+        int[] dr = {1, 0}; // y,m 방향
+        
+        for(int i=0; i<2; i++) {
+            int nc = x+dc[i];
+            int nr = y+dr[i];
+            
+            // 좌표 이내, grid 값 1, visited 아님
+            if(0<=nc && nc<n && 0<=nr && nr<m) {
+                if(grid[nc][nr]==1 && (visited[nc][nr]==0)) {
+                    dfs(grid, nc, nr, n, m, visited);
+                }
+            }
+        }
+        
+    }
+}
